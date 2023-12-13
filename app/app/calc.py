dec043f711de6bec3bfef8d32b83c3806ab31159
clrinc <127432082+clrinc@users.noreply.github.com>
2023-12-13 16:30:06 -0500

    Configured docker compose and checks to wait for db

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+calculator functions
+"""
+
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """add x and y and return result"""
+    return y - x
